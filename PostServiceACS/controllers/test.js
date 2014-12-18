e4441816eac759e239bcbf541be8afbef5f6998f
John Jardin <john.jardin@gmail.com>
2014-12-18 08:16:34 +0200

    Aaron Saunders Fix to ACS Web Service

--- PostServiceACS/controllers/test.js
+++ PostServiceACS/controllers/test.js
@@ -1,18 +1,11 @@
 function TestPostService(req, res) {
-	console.log("Start running of TestPostService");
-	var body = "";
-	
-	req.on('data', function(data) {
-        console.log('Data Request');
-        body += data;
-    });
-
-	req.on('end', function() {
-		console.log("Start Printing Body");
-		console.log(body);
-		console.log("End Printing Body");
-		
-		res.render('index', { title: 'TestPostService' });
-		console.log("End running of TestPostService");
-    });
+    console.log("Start running of TestPostService");
+    var body = "";
+ 
+    console.log("Start Printing Body");
+    console.log(req.body);
+    console.log("End Printing Body");
+ 
+    res.render('index', { title: 'TestPostService' });
+    console.log("End running of TestPostService");
 }
